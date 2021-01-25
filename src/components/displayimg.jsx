a0ee389a77696575909d1e22d79686376699cb95
Andy Rodgers <arr2060@gmail.com>
2021-01-25 00:49:35 -0800

    Refactored components that were unnecessarily class into functional

--- src/components/displayimg.jsx
+++ src/components/displayimg.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class DisplayImg extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    const { photo } = this.props;
-    return (
-      <div className="display-image">
-        <img src={photo} id="displayimg" alt="" />
-      </div>
-    );
-  }
-}
+// eslint-disable-next-line arrow-body-style
+const DisplayImg = ({ photo }) => {
+  return (
+    <div className="display-image">
+      <img src={photo} id="displayimg" alt="" />
+    </div>
+  );
+};
 
 DisplayImg.propTypes = {
   photo: PropTypes.string.isRequired,
--- src/components/purchasebox.jsx
+++ src/components/purchasebox.jsx
@@ -1,54 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class PurchaseBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  render() {
-    const { quantity, price } = this.props;
-    return (
-      <div className="purchase-box">
-        <div className="curbside">
-          Avoid shipping delays, choose Curbside Pickup available at select locations.
-          <br />
-          <a href="#0">Learn more</a>
-        </div>
-        <div>
-          <form>
-            <label htmlFor="ship">
-              <input type="radio" id="ship" name="shipOpt" />
-              &nbsp;Ship to address
-            </label>
-            <br />
-            <label htmlFor="curb">
-              <input type="radio" id="curb" name="shipOpt" checked />
-              &nbsp;Pick up at store
-              <br />
-              <a href="#0" className="no-bold">Find a store near you</a>
-            </label>
-            <br />
-            <button type="submit" className="cart-button">
-              Add to cart &mdash; $
-              {(quantity * price).toFixed(2)}
-            </button>
-            <br />
-            <button type="button" className="list-button">Add to wish list</button>
-            <button type="button" className="list-button">Add to registry</button>
-          </form>
-        </div>
-        <span>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <a href="#0">REI return policy</a>
-        </span>
+// eslint-disable-next-line arrow-body-style
+const PurchaseBox = ({ quantity, price }) => {
+  return (
+    <div className="purchase-box">
+      <div className="curbside">
+        Avoid shipping delays, choose Curbside Pickup available at select locations.
+        <br />
+        <a href="#0">Learn more</a>
       </div>
-    );
-  }
-}
+      <div>
+        <form>
+          <label htmlFor="ship">
+            <input type="radio" id="ship" name="shipOpt" />
+            &nbsp;Ship to address
+          </label>
+          <br />
+          <label htmlFor="curb">
+            <input type="radio" id="curb" name="shipOpt" checked />
+            &nbsp;Pick up at store
+            <br />
+            <a href="#0" className="no-bold">Find a store near you</a>
+          </label>
+          <br />
+          <button type="submit" className="cart-button">
+            Add to cart &mdash; $
+            {(quantity * price).toFixed(2)}
+          </button>
+          <br />
+          <button type="button" className="list-button">Add to wish list</button>
+          <button type="button" className="list-button">Add to registry</button>
+        </form>
+      </div>
+      <span>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#0">REI return policy</a>
+      </span>
+    </div>
+  );
+};
 
 PurchaseBox.propTypes = {
   quantity: PropTypes.number.isRequired,
